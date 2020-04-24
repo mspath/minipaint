438aedf1d496296ade36d8bde3756bea438c1ec9
Markus Spath <markus@hackr.de>
2020-04-24 08:32:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Minipaint
+=========
+
+A Simple line drawing app.
+
+One of the toy apps of Google's [Developing Android Apps with Kotlin](https://www.udacity.com/course/developing-android-apps-with-kotlin--ud9012) at Udacity.
+
+Technologies
+------------
+
+* build a custom View
+* drawing a View
+* listening to motion Events
+* programatically adding the View to the activity
+ 
---
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -0,0 +1,121 @@
+package com.example.minipaint
+
+import android.content.Context
+import android.graphics.*
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
+
+private const val STROKE_WIDTH = 12f
+
+class MyCanvasView(context: Context) : View(context) {
+
+//    private lateinit var extraCanvas: Canvas
+//    private lateinit var extraBitmap: Bitmap
+
+    private var counter = 0
+
+    // Path representing the drawing so far
+    private var drawing = Path()
+
+    // Path representing what's currently being drawn
+    private var path = Path()
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private lateinit var frame: Rect
+
+    private val drawColor = ResourcesCompat
+        .getColor(resources, R.color.colorPaint, null)
+
+    private val backgroundColor = ResourcesCompat
+        .getColor(resources, R.color.colorBackground, null)
+
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width
+    }
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+//        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+//
+//        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        extraCanvas = Canvas(extraBitmap)
+//        extraCanvas.drawColor(backgroundColor)
+
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+//        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+
+        // draw the background
+        canvas.drawColor(backgroundColor)
+
+        // Draw the drawing so far
+        canvas.drawPath(drawing, paint)
+
+        // draw the current new path
+        canvas.drawPath(path, paint)
+
+        // draw the frame
+        canvas.drawRect(frame, paint)
+
+        //Log.d("MyCanvasView", "ondraw called ${counter++}")
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            // extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        drawing.addPath(path)
+        path.reset()
+    }
+}
