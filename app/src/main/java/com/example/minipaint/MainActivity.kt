33182a534cee43f8747b4998a60c26dd922e1b44
Markus Spath <markus@hackr.de>
2023-03-13 17:36:56 +0100

    fix deprecated imports

--- app/src/main/java/com/example/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/minipaint/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+//import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val myCanvasView = MyCanvasView(this)
-        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        //myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
         setContentView(myCanvasView)
     }
